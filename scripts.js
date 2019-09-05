8f265ae9ce45473e7dc68d3220c22f59d3b89477
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2019-09-05 14:29:49 +0100

    Fix form submission

--- scripts.js
+++ scripts.js
@@ -1,11 +1,17 @@
 (function app() {
 	let form = document.querySelector("#form");
+	let formtag = document.querySelector("form");
 	let contact = document.querySelector("#contact");
 	
 	form.addEventListener('click', function closeform(e) {
 		if (e.target == form) {
 			form.classList.remove("open");
 		}
+	});
+	
+	form.addEventListener('submit', function validate(e) {
+		e.preventDefault();
+		form.classList.remove("open");
 	});
 	
 	contact.addEventListener('click', function openform(e) {
