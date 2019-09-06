5969230eee8ca27239de1ef9d15620951a69a35e
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2019-09-06 18:14:55 +0100

    Make PHP save submitted values to file.

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@
 	let form = document.querySelector("#form");
 	let formtag = document.querySelector("form");
 	let contact = document.querySelector("#contact");
+	let message = {};
 	
 	form.addEventListener('click', function closeform(e) {
 		if (e.target == form) {
@@ -9,8 +10,36 @@
 		}
 	});
 	
-	form.addEventListener('submit', function validate(e) {
+	async function sendForm() {
+		const rawResponse = await fetch('./submit.php', {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(message)
+		});
+		let content;
+		try {
+			content = await rawResponse.clone().json();
+		} catch (e) {
+			content = await rawResponse.text();
+		}
+		
+		console.log(content);
+	}
+	
+	formtag.addEventListener('submit', function validate(e) {
 		e.preventDefault();
+		
+		message.name = formtag.querySelector("input[name='name']").value;
+		message.email = formtag.querySelector("input[name='email']").value;
+		message.title = formtag.querySelector("input[name='title']").value;
+		message.message = formtag.querySelector("textarea").value;
+		
+		sendForm(message);
+		
+		
 		formtag.classList.add("filled");
 		setTimeout(function closeform() {
 			form.classList.remove("open");
